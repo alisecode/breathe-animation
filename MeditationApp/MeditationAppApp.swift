42789821e0da91fe71039992e0addab49233f895
Alisa <alisa.melissa@icloud.com>
2024-04-05 16:52:40 +0300

    Initial Commit

---
+++ MeditationApp/MeditationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeditationAppApp.swift
+//  MeditationApp
+//
+//  Created by Алиса Третьякова on 05.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct MeditationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
