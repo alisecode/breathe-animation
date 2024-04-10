32800b490a0e5caf1936e85b5297f82348a7cd8a
Alisa <alisa.melissa@icloud.com>
2024-04-10 15:40:35 +0300

    commit

---
+++ MeditationApp/BreatheAnimation.swift
@@ -0,0 +1,18 @@
+//
+//  BreatheAnimation.swift
+//  MeditationApp
+//
+//  Created by Алиса Третьякова on 05.04.2024.
+//
+
+import SwiftUI
+
+struct BreatheAnimation: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BreatheAnimation()
+}
---
+++ MeditationApp/BreatheModel.swift
@@ -0,0 +1,8 @@
+//
+//  BreatheModel.swift
+//  MeditationApp
+//
+//  Created by Алиса Третьякова on 05.04.2024.
+//
+
+import Foundation
